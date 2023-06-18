17bc169b5389bb32d119ab6bcc8fb0f2c9b9fb09
Cem <cemicyz@protonmail.com>
2023-06-18 17:07:59 +0300

    Initial commit

---
+++ src/main/java/me/yungcemic/sakuraauction/SakuraAuctionPlugin.java
@@ -0,0 +1,16 @@
+package me.yungcemic.sakuraauction;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SakuraAuctionPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
